92a62b17650ac4475979fc8d80fe8a0cdd7f9d3c
macsnewcalling <sabthik85@gmail.com>
2025-06-23 07:05:54 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,17 @@
-a gh
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_ccs5fa1C6x8UGJORrWJv0aLMN0geO8I",
+  authDomain: "macsstop1.firebaseapp.com",
+  projectId: "macsstop1",
+  storageBucket: "macsstop1.firebasestorage.app",
+  messagingSenderId: "925533128989",
+  appId: "1:925533128989:web:33ea844a99106a453c79f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
